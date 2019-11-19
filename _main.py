480a40311cda940a97024160a40a935bbd8c3442
Mike Roberts <mike.roberts.2k10@googlemail.com>
2019-11-19 11:03:21 +0000

    Skeleton breathe command set

---
+++ _main.py
@@ -0,0 +1,13 @@
+from my_commands.imports import *
+
+modules = {
+    "my_commands": {
+        "apps": [],
+        "core": [
+            "keys",
+        ],
+        "languages": [],
+    }
+}
+
+Breathe.load_modules(modules)
