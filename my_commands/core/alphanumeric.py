4c6252440e90daf73e92e03b138c12a7f782ea12
Mike Roberts <mike.roberts.2k10@googlemail.com>
2019-12-13 10:20:22 +0000

    Numbers

--- my_commands/core/alphanumeric.py
+++ my_commands/core/alphanumeric.py
@@ -35,9 +35,11 @@ Breathe.add_commands(
         "[<big>] <letter>": Function(
             lambda big, letter: Text(letter.upper() if big else letter).execute()
         ),
+        "numb <num_seq>": Function(lambda num_seq: Text("".join(map(str, num_seq)))),
     },
     [
         Choice("big", {"big": True}, default=False),
         Choice("letter", nato_alphabet),
+        Repetition(IntegerRef("", 0, 10), min=1, max=6, name="num_seq"),
     ]
 )
