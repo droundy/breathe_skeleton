e279b8b4c16a62ed5a2b96e710a6f3d628d05fe3
David Roundy <roundyd@physics.oregonstate.edu>
2020-05-27 14:54:25 -0700

    add shift-arrow commands

--- my_commands/core/keys.py
+++ my_commands/core/keys.py
@@ -7,6 +7,12 @@ Breathe.add_commands(
         "<n> right":         Key("right:%(n)d"),
         "<n> up":            Key("up:%(n)d"),
         "<n> down": Key("down:%(n)d"),
+
+        "<n> lefts":          Key("s-left:%(n)d"),
+        "<n> rights":         Key("s-right:%(n)d"),
+        "<n> ups":            Key("s-up:%(n)d"),
+        "<n> downs": Key("s-down:%(n)d"),
+
         "[<n>] page down": Key("pgdown:%(n)d"),
         "[<n>] page up": Key("pgup:%(n)d"),
     },
