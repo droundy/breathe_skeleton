9fc8efee1ce88f901f9f2bba4c4cc57e566e52af
David Roundy <roundyd@physics.oregonstate.edu>
2020-05-27 11:09:02 -0700

    refine python a bit

--- my_commands/languages/python.py
+++ my_commands/languages/python.py
@@ -1,32 +1,32 @@
 from dragonfly import Dictation, AppContext, Text, Key
 from breathe import Breathe, CommandContext
 
+keywords = [
+    'for', 'in',
+    'if', 'else',
+    'import', 'from', 'def', 'class', 'pass', 'return',
+]
+functions = ['range', 'print']
+
+mapping = {
+    'colon': Text(':\n'),
+    'comma': Text(', '),
+    '(ell if|else if)': Text('elif '),
+    "snake case [<snaketext>]" : Text("%(snaketext)s"),
+    "Camel Case [<CamelText>]"  : Text("%(CamelText)s"),
+}
+for k in keywords:
+    mapping[k] = Text(k + ' ');
+for k in functions:
+    mapping[k] = Text(k + '()') + Key('left');
+
 Breathe.add_commands(
     # Commands will be active either when we are editing a python file
     # or after we say "enable python". pass None for the commands to be global.
     context = AppContext(title=".py") | CommandContext("python"),
-    mapping = {
-        "for each"              : Text("for  in :") + Key("left:5"),
-        "for loop"              : Text("for i in range():") + Key("left:2"),
-        "from import"           : Text("from  import ") + Key("home, right:5"),
-        "function"              : Text("def ():") + Key("left:3"),
-        "(iffae | iffy)"        : Text("if :") + Key("left"),
-        "iffae not"             : Text("if not :") + Key("left"),
-        "import"                : Text("import "),
-        "lambda"                : Text("lambda :") + Key("left"),
-        "while loop"            : Text("while :") + Key("left"),
-        "shell iffae"           : Text("elif :") + Key("left"),
-        "shells"                : Text("else:"),
-        "return"                : Text("return "),
-        # ------------------------------------------------
-        "method <snaketext>"    : Text("def %(snaketext)s(self):") + Key("left:2"),
-        "function [<snaketext>]": Text("def %(snaketext)s():") + Key("left:2"),
-        "selfie [<snaketext>]"  : Text("self.%(snaketext)s"),
-        "pointer [<snaketext>]" : Text(".%(snaketext)s"),
-        "classy [<classtext>]"  : Text("class %(classtext)s:") + Key("left"),
-    },
+    mapping = mapping,
     extras = [
         Dictation("snaketext", default="").lower().replace(" ", "_"),
-        Dictation("classtext", default="").title().replace(" ", ""),
+        Dictation("CamelText", default="").title().replace(" ", ""),
     ]
 )
