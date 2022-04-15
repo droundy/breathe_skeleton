33397f86527266085275a5780df9efba1c587068
David Roundy <roundyd@physics.oregonstate.edu>
2022-04-15 12:23:46 -0700

    automagically pick desktop environment

--- _main.py
+++ _main.py
@@ -1,7 +1,28 @@
 from my_commands.imports import Breathe
-import logging
+import logging, os, sys
 logging.basicConfig()
 
+#From http://stackoverflow.com/questions/2035657/what-is-my-current-desktop-environment
+desktop = 'unknown'
+if sys.platform in ["win32", "cygwin"]:
+    desktop = "windows"
+elif sys.platform == "darwin":
+    desktop = "mac"
+else: #Most likely either a POSIX system or something not much common
+    desktop_session = os.environ.get("DESKTOP_SESSION")
+    print(f"desktop_session is {desktop_session}")
+    if 'gnome' in desktop_session:
+        desktop = 'gnome'
+    elif 'kde' in desktop_session:
+        desktop = 'kde'
+
+desktop_fname = f'my_commands/core/{desktop}.py'
+if not os.path.exists(desktop_fname):
+    print('Desktop {desktop} is unsupported.  Please create {desktop_fname}!')
+    exit()
+
+if desktop not in ['gnome', 'kde']:
+    print('unsupported desktop: ')
 
 modules = {
     "my_commands": {
@@ -14,8 +35,7 @@ modules = {
         ],
         "core": [
             "keys",
-            # "kde",
-            "gnome",
+            desktop,
             # "alphanumeric",
         ],
         "languages": [
